1d4958761463f6f6e27554c6f8bfb4ba582ad7d8
BlueSky <s1311422694@foxmail.com>
2021-04-18 19:30:48 +0800

    kotlin 定语与目的

---
+++ app/src/main/java/com/app/kotlinlearning/1.Kotlin定义和目的.kt
@@ -0,0 +1,15 @@
+package com.app.kotlinlearning
+
+/**
+ *  @author strugglelin
+ *  @date 2021/4/18
+ *  description:
+ */
+/*
+1、kotlin 是一种静态类型编程语言（编译检测），groovy动态类型编程语言（运行对字段方法等检测）
+2、函数式和面向对象语言。函数式：把函数当做值使用，保存、传递、返回。面向对象：万物皆对象
+3、简洁：代码简洁，丰富的标准库
+4、安全：类型推断、null 检测等
+5、互操作性：和 java 互相调用
+
+*/
---
+++ app/src/main/java/com/app/kotlinlearning/2.Kotlin基础.kt
@@ -0,0 +1,8 @@
+package com.app.kotlinlearning
+
+/**
+ *  @author strugglelin
+ *  @date 2021/4/18
+ *  description:
+ */
+// 1、函数和变量
